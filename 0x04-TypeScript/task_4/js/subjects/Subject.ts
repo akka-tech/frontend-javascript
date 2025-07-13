927abbf5a59253af8bfbac88d83b4c2043638b15
akka-tech <akkaabdlwahid3@gmail.com>
2025-07-13 19:54:18 +0000

    add task_4

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,18 @@
+namespace Subjects {
+  export class Subject {
+    private _teacher!: Teacher;
+
+    set teacher(teacher: Teacher) {
+      this._teacher = teacher;
+    }
+
+    get teacher(): Teacher {
+      return this._teacher;
+    }
+
+    setTeacher(teacher: Teacher): void {
+      this._teacher = teacher;
+    }
+  }
+}
+
